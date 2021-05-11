673e8edc30f9333dbfcea5065fa211316ebec8b7
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-11 22:45:01 +0300

    Created layers.

---
+++ lib/views/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static const String id = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
