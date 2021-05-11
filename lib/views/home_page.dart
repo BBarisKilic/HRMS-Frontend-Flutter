4b1863c6493ccd78a346263eacf0b6b663835396
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-11 22:59:55 +0300

    Added Web support.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.blue,
+    );
   }
 }
