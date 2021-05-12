a21e7dc0e5561d62f20c16a8e9f5aedde8869f7f
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-12 21:33:16 +0300

    Added new assets for login screen.

--- lib/views/login/login_page_phone.dart
+++ lib/views/login/login_page_phone.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:hrms_ui/components/background_clipper.dart';
 import 'package:hrms_ui/views/login/login_page.dart';
@@ -15,9 +16,7 @@ class LoginPagePhone extends StatelessWidget {
           width: 100.w,
           color: const Color(0xff8286F4),
           child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Stack(
               children: [
                 IconButton(
                   onPressed: () {
@@ -26,6 +25,43 @@ class LoginPagePhone extends StatelessWidget {
                   icon: const Icon(
                     Icons.arrow_back_ios_rounded,
                     color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 4.h),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(
+                      "assets/images/headhunter.svg",
+                      height: 12.h,
+                      color: const Color(0xff4d59c1),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: SvgPicture.asset(
+                      "assets/images/recruitment.svg",
+                      height: 12.h,
+                      color: const Color(0xff4d59c1),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, bottom: 6.h),
+                  child: Row(
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: SvgPicture.asset(
+                          "assets/images/employee.svg",
+                          height: 12.h,
+                          color: const Color(0xff4d59c1),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
